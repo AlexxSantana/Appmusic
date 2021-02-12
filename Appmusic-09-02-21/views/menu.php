8297c316f9ef8c816f86cbb2256be8cdf9002f63
AlexxSantana <71818252+AlexxSantana@users.noreply.github.com>
2021-02-12 17:00:43 +0100

    Update menu.php
    Modificada la ruta de dowmusic_view.php

--- Appmusic-09-02-21/views/menu.php
+++ Appmusic-09-02-21/views/menu.php
@@ -21,7 +21,7 @@ if (isset($_COOKIE) && isset($_COOKIE["user"]) === false) {
 <body>
 	<h1>Bienvenido <?php echo htmlspecialchars($_COOKIE["user"]) ?></h1>
 	<li><a href="./../controllers/logout.php">Cerrar sesión</a></li>
-	<li><a href="downmusic.php">Descargar musica</a></li>
+	<li><a href="downmusic_view.php">Descargar musica</a></li>
 	<li><a href="histfacturas.php">Consultar historial de facturas</a></li>
 	<li><a href="facturas.php">Consultar facturas entre dos fechas</a></li>
 	<li><a href="ranking.php">Consultar descargas entre dos fechas</a></li>
